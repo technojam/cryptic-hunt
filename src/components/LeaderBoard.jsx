f667bac098e3f85a557d697d0af0ef325d1a677f
YASH SINGH <123338008+yashsingh9651@users.noreply.github.com>
2024-04-04 10:53:36 +0530

    Leaderboard Data fetched

--- src/components/LeaderBoard.jsx
+++ src/components/LeaderBoard.jsx
@@ -1,129 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Loader from "./Loader"
+import axios from "axios";
 
 function LeaderBoard() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios.post(`http://localhost:3000/`,{"action":"getUsers"})
+      .then(res => {
+        const data = res.data;
+        if (data.status == 200) {
+          setLoading(false);
+        }
+        setUsers(data.users);
+      })
+  }, [])
+  
   return (
-    <div className="flex scrollbar flex-col gap-2 bg-[#111416]  h-4/5 w-1/5 rounded-lg">
+    <div className="flex scrollbar flex-col gap-2 bg-[#111416] h-4/5 items-center w-1/5 rounded-lg">
       <h1 className="text-center my-3 font-semibold text-2xl text-white">Leaderboard</h1>
-
-      <div className="flex flex-col gap-3 overflow-y-scroll h-5/6 rounded-lg items-center ">
-        <div className="flex items-center bg-slate-600 w-[96%] py-2 rounded-2xl">
+      {!loading?<div className="flex flex-col gap-3 overflow-y-scroll w-full h-5/6 rounded-lg items-center ">
+        {users.map((user)=>{
+        return <div key={user._id} className="flex items-center bg-slate-600 w-[96%] py-2 rounded-2xl">
           <img
             className="w-7 h-7 ml-2 mr-3"
             src="https://res.cloudinary.com/dyfhbqtjm/image/upload/f_auto,q_auto/nvpkmaabzlvihppq7d0o"
             alt=""
           />
           <div className="flex w-full justify-between items-center">
-            <div className="text-white font-semibold">Name</div>
-            <div className="rounded-2xl bg-white p-1 mx-2">
-              <p>5000</p>
+            <div className="text-white font-semibold">{user.name}</div>
+            <div className="rounded-full aspect-square w-8 text-sm flex justify-center items-center bg-white mx-2">
+              <p>{user.points}</p>
             </div>
           </div>
-        </div>
-        <div className="flex items-center bg-slate-600 w-[96%] py-2 rounded-2xl">
-          <img
-            className="w-7 h-7 ml-2 mr-3"
-            src="https://res.cloudinary.com/dyfhbqtjm/image/upload/f_auto,q_auto/nvpkmaabzlvihppq7d0o"
-            alt=""
-          />
-          <div className="flex w-full justify-between items-center">
-            <div className="text-white font-semibold">Name</div>
-            <div className="rounded-2xl bg-white p-1 mx-2">
-              <p>5000</p>
-            </div>
-          </div>
-        </div>
-        <div className="flex items-center bg-slate-600 w-[96%] py-2 rounded-2xl">
-          <img
-            className="w-7 h-7 ml-2 mr-3"
-            src="https://res.cloudinary.com/dyfhbqtjm/image/upload/f_auto,q_auto/nvpkmaabzlvihppq7d0o"
-            alt=""
-          />
-          <div className="flex w-full justify-between items-center">
-            <div className="text-white font-semibold">Name</div>
-            <div className="rounded-2xl bg-white p-1 mx-2">
-              <p>5000</p>
-            </div>
-          </div>
-        </div>
-        <div className="flex items-center bg-slate-600 w-[96%] py-2 rounded-2xl">
-          <img
-            className="w-7 h-7 ml-2 mr-3"
-            src="https://res.cloudinary.com/dyfhbqtjm/image/upload/f_auto,q_auto/nvpkmaabzlvihppq7d0o"
-            alt=""
-          />
-          <div className="flex w-full justify-between items-center">
-            <div className="text-white font-semibold">Name</div>
-            <div className="rounded-2xl bg-white p-1 mx-2">
-              <p>5000</p>
-            </div>
-          </div>
-        </div>
-        <div className="flex items-center bg-slate-600 w-[96%] py-2 rounded-2xl">
-          <img
-            className="w-7 h-7 ml-2 mr-3"
-            src="https://res.cloudinary.com/dyfhbqtjm/image/upload/f_auto,q_auto/nvpkmaabzlvihppq7d0o"
-            alt=""
-          />
-          <div className="flex w-full justify-between items-center">
-            <div className="text-white font-semibold">Name</div>
-            <div className="rounded-2xl bg-white p-1 mx-2">
-              <p>5000</p>
-            </div>
-          </div>
-        </div>
-        <div className="flex items-center bg-slate-600 w-[96%] py-2 rounded-2xl">
-          <img
-            className="w-7 h-7 ml-2 mr-3"
-            src="https://res.cloudinary.com/dyfhbqtjm/image/upload/f_auto,q_auto/nvpkmaabzlvihppq7d0o"
-            alt=""
-          />
-          <div className="flex w-full justify-between items-center">
-            <div className="text-white font-semibold">Name</div>
-            <div className="rounded-2xl bg-white p-1 mx-2">
-              <p>5000</p>
-            </div>
-          </div>
-        </div>
-        <div className="flex items-center bg-slate-600 w-[96%] py-2 rounded-2xl">
-          <img
-            className="w-7 h-7 ml-2 mr-3"
-            src="https://res.cloudinary.com/dyfhbqtjm/image/upload/f_auto,q_auto/nvpkmaabzlvihppq7d0o"
-            alt=""
-          />
-          <div className="flex w-full justify-between items-center">
-            <div className="text-white font-semibold">Name</div>
-            <div className="rounded-2xl bg-white p-1 mx-2">
-              <p>5000</p>
-            </div>
-          </div>
-        </div>
-        <div className="flex items-center bg-slate-600 w-[96%] py-2 rounded-2xl">
-          <img
-            className="w-7 h-7 ml-2 mr-3"
-            src="https://res.cloudinary.com/dyfhbqtjm/image/upload/f_auto,q_auto/nvpkmaabzlvihppq7d0o"
-            alt=""
-          />
-          <div className="flex w-full justify-between items-center">
-            <div className="text-white font-semibold">Name</div>
-            <div className="rounded-2xl bg-white p-1 mx-2">
-              <p>5000</p>
-            </div>
-          </div>
-        </div>
-        <div className="flex items-center bg-slate-600 w-[96%] py-2 rounded-2xl">
-          <img
-            className="w-7 h-7 ml-2 mr-3"
-            src="https://res.cloudinary.com/dyfhbqtjm/image/upload/f_auto,q_auto/nvpkmaabzlvihppq7d0o"
-            alt=""
-          />
-          <div className="flex w-full justify-between items-center">
-            <div className="text-white font-semibold">Name</div>
-            <div className="rounded-2xl bg-white p-1 mx-2">
-              <p>5000</p>
-            </div>
-          </div>
-        </div>
-      </div>
+        </div>})}
+      </div>:<Loader />}
     </div>
   );
 }
